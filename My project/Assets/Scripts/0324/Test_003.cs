151c0e53c2f64f5b8b7d7bebd771b3244b94d083
mirecaf <neveah204@gmail.com>
2023-03-24 11:04:30 +0900

    if 문, for문 사용

--- My project/Assets/Scripts/0324/Test_003.cs
+++ My project/Assets/Scripts/0324/Test_003.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Test_003 : MonoBehaviour
 {
+    public int herbNum = 1;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("HelloWorld");
+        
+
+        if (herbNum == 1)
+            Debug.Log("체력을 50 회복");
+        else
+            Debug.Log("체력을 -50 ");
+
     }
 
     // Update is called once per frame
